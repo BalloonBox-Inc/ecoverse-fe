ead693056f30b47409d4322c61b85f6ede5bef06
jekiquin-bb <jek@balloonbox.io>
2022-11-15 10:19:31 -0800

    added theme for daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#ce0a76',
+          secondary: '#6d2caa',
+          accent: '#c100fc',
+          neutral: '#161D22',
+          'base-100': '#2D345D',
+          info: '#7ABBDC',
+          success: '#60EBDD',
+          warning: '#A5660D',
+          error: '#F6517A',
+        },
+      },
+    ],
+  },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
