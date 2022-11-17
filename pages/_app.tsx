3a4c973df59ac60fa23911eb8705c3e7767cc216
jekiquin-bb <jek@balloonbox.io>
2022-11-17 11:51:25 -0800

    added queryclient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@styles/globals.css';
+
+import AuthProvider from '@context/auth';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
