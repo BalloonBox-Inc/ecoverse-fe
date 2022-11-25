82aa97f014122ba75c00c19d1b2a26e3336c2c2b
jekiquin-bb <jek@balloonbox.io>
2022-11-25 15:06:07 -0800

    changed the styling on 404 pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -24,7 +24,7 @@ export default function Custom404() {
 }
 
 const styles = {
-  root: 'h-screen w-screen bg-404 bg-cover bg-bottom relative',
+  root: 'h-screen w-screen bg-404 bg-cover bg-bottom relative text-base-100',
   main: 'w-11/12 max-w-lg absolute top-24 left-2/4 -translate-x-2/4 flex flex-col items-center gap-10 bg-neutral/70 px-4 py-10 rounded-lg shadow-xl backdrop-blur-lg',
   iconContainer: 'w-20 h-20',
   icon: 'fill-error',
