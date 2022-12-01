dc4ac1f53e8f371a579987b10c90304c221a4fab
jekiquin-bb <jek@balloonbox.io>
2022-11-30 16:31:56 -0800

    added secondary action for navlist

--- utils/side-navigation.ts
+++ utils/side-navigation.ts
@@ -2,30 +2,52 @@ import HelpIcon from '@components/Icons/HelpIcon';
 import MapIcon from '@components/Icons/MapIcon';
 import MyForestsIcon from '@components/Icons/MyForestsIcon';
 import ProjectsIcon from '@components/Icons/ProjectsIcon';
+import SearchIcon from '@components/Icons/SearchIcon';
 
 export const navList = [
   {
     label: 'Map',
     Icon: MapIcon,
+    action: 'link',
     href: '/',
   },
   {
     label: 'Projects',
     Icon: ProjectsIcon,
+    action: 'link',
     href: '/projects',
   },
   {
     label: 'My Forests',
     Icon: MyForestsIcon,
+    action: 'link',
     href: '/forests',
   },
   {
     label: 'Help',
     Icon: HelpIcon,
     href: '/help',
+    action: 'link',
   },
 ];
 
 Object.freeze(navList);
 
-export type NavItem = typeof navList[0];
+export const navListSecondary = [
+  {
+    label: 'Search',
+    Icon: SearchIcon,
+    href: null,
+  },
+  // {
+  //   label: 'Selected Tiles',
+  //   Icon: ,
+  //   action: 'modal',
+  //   href: null,
+  // },
+];
+
+// export type NavItem = typeof navList[0];
+export type NavItem = Omit<typeof navList[0], 'action'> & {
+  action: string | Function;
+};
