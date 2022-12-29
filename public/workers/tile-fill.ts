889d1cd6ce2abbd0c025931ac09a5c0b5dcea4d3
jekiquin-bb <jek@balloonbox.io>
2022-12-29 12:49:40 -0800

    added logic when no batch selection

--- public/workers/tile-fill.ts
+++ public/workers/tile-fill.ts
@@ -4,6 +4,12 @@ import * as mapUtils from '@utils/map-utils';
 
 onmessage = (e: MessageEvent<TileObj[]>) => {
   const selectedTiles = e.data;
+  
+  if (selectedTiles.length === 0) {
+    postMessage([])
+    return;
+  }
+  
   const centers = selectedTiles.map((tile) => {
     const polygon = mapUtils.getPolygonFromTile(tile);
     return mapUtils.getCenterCoordsFromPolygon(polygon);
