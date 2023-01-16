37b6f44d17b9491527ad9f06c2cfbcd00faa6b82
jekiquin-bb <jek@balloonbox.io>
2023-01-16 12:47:23 -0800

    added purchase tiles state for purchse slice

--- plugins/store/slices/purchase.ts
+++ plugins/store/slices/purchase.ts
@@ -1,19 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '@plugins/store';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TileObj } from '@utils/interface/map-interface';
 
 export type PurchaseState = {
   isPurchasing: boolean;
+  tilesToPurchase: TileObj[];
 };
 
 const initialState: PurchaseState = {
   isPurchasing: false,
+  tilesToPurchase: [],
 };
 
 const purchaseSlice = createSlice({
   name: 'purchase',
   initialState,
-  reducers: {},
+  reducers: {
+    startPurchasing: (state) => {
+      state.isPurchasing = true;
+    },
+    stopPurchasing: (state) => {
+      state.isPurchasing = false;
+    },
+    setIsPurchasing: (state, action: PayloadAction<boolean>) => {
+      state.isPurchasing = action.payload;
+    },
+    setTilesToPurchase: (state, action: PayloadAction<TileObj[]>) => {
+      state.tilesToPurchase = action.payload;
+    },
+    clearTilesToPurchase: (state) => {
+      state.tilesToPurchase = [];
+    },
+  },
 });
 
-export const {} = purchaseSlice.actions;
+export const { startPurchasing, stopPurchasing, setTilesToPurchase } =
+  purchaseSlice.actions;
 
 export default purchaseSlice.reducer;
+
+export const selectIsPurchasing = (state: RootState) =>
+  state.purchase.isPurchasing;
+
+export const selectTilesToPurchase = (state: RootState) =>
+  state.purchase.tilesToPurchase;
